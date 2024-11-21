e325a72a1cc8ee7bebee64f59a47b38d620917a9
vedant1711 <vscode1711@gmail.com>
2024-11-21 18:46:05 +0530

    added cors headers

--- bajajtest/settings.py
+++ bajajtest/settings.py
@@ -27,6 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # For React/Next.js local development
+    "https://yourdomain.com",  # Your production domain
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 # Application definition
 
@@ -38,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'app',
 ]
 
@@ -49,6 +57,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'bajajtest.urls'
